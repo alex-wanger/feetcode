20d0799b906eef9e931f78f1a76b49ac87ec63d6
Mekot S <mekotsarder@gmail.com>
2025-06-11 16:42:41 -0400

    main content pages done

--- html/script.js
+++ html/script.js
@@ -19,3 +19,30 @@ fetch('../html/footer.html')
     document.getElementById('footer-container').innerHTML = data;
   });
   
+
+  const images = [
+    { src: "../images/scatterplot.png", caption: "Scatter Plot" },
+    { src: "../images/bargraph.png", caption: "Bar Graph" },
+    { src: "../images/piechart.png", caption: "Pie Chart" }
+  ];
+
+  let currentIndex = 0;
+
+  function updateCarousel() {
+    const img = document.getElementById("carousel-img");
+    const caption = document.getElementById("carousel-caption");
+
+    img.src = images[currentIndex].src;
+    img.alt = images[currentIndex].caption;
+    caption.textContent = images[currentIndex].caption;
+  }
+
+  function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateCarousel();
+  }
+
+  function prevImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateCarousel();
+  }
